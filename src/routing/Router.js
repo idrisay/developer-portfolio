e3113bba89c0ef45c7503344a419c577e56b05e4
Idris AY <97283155+idrisay@users.noreply.github.com>
2022-12-17 12:21:31 +0100

    feat: add mon height

--- src/routing/Router.js
+++ src/routing/Router.js
@@ -1,4 +1,4 @@
-import Home  from "../pages/Home";
+import Home from "../pages/Home";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "../pages/About";
@@ -9,13 +9,15 @@ import Footer from "../components/Footer";
 const Router = () => {
   return (
     <BrowserRouter>
-    <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-      <Footer/>
+      <Navbar />
+      <div style={{ minHeight: "80vh" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </div>
+      <Footer />
     </BrowserRouter>
   );
 };
